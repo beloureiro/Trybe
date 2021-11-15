96374fe31f36667e8a29c9dc3ade39953487e56b
beloureiro <beloureiro@gmail.com>
2021-11-15 19:40:53 -0300

    initial commit

---
+++ trybe-exercicios/Desafio_Logica_112021/desafio.js
@@ -0,0 +1,29 @@
+//----DESAFIO 1-----
+function conversor(f){
+var resultado = (f - 32)/1.8
+return console.log(resultado)
+}
+conversor(10)
+//----DESAFIO 2-----
+function resultadoX(x){
+if(x>10 ){ console.log(x-10)
+    x-10
+} else {
+    console.log(10-x)
+}}
+resultadoX(7)
+//----DESAFIO 3-----
+function acheonumero(s){
+var numero =1
+var s = 26
+for(var x=0;x<=1000;x++){
+{if (numero+x==26) console.log(x)}}
+}
+acheonumero(26)
+
+
+
+
+
+
+
